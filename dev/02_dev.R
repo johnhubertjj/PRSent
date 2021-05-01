b74b3f0163c625b3c504bcb8beedd53c644cf615
= <jj@vmjh.com>
2021-05-01 11:37:18 +0100

    pre-release version ready for deployment

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -17,6 +17,8 @@
 ## Add one line by package you want to add as dependency
 usethis::use_package( "thinkr" )
 usethis::use_package( "waiter")
+usethis::use_package( "rlang" )
+usethis::use_package( "shinyjs" )
 
 ## Add modules ----
 ## Create a module infrastructure in R/
