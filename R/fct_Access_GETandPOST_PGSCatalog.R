b3b5d3d5a433590183e48bbc07d6084211448dcc
= <jj@vmjh.com>
2021-05-06 14:18:18 +0100

    Added in the methodology to get the meta-data and the ftp site

--- R/fct_Access_GETandPOST_PGSCatalog.R
+++ R/fct_Access_GETandPOST_PGSCatalog.R
@@ -5,68 +5,10 @@
 #' @return The return value, if any, from executing the function.
 #'
 #' @noRd
-get_and_post <- function(){
-  api_url <- session$registerDataObj( 
-    name   = 'api', # an arbitrary but unique name for the data object
-    data   = list(), # you can bind some data here, which is the data argument for the
-    # filter function below.
-    
-    filter = function(data, req) {
-      print(ls(req))  # you can inspect what variables are encapsulated in this req
-      # environment
-      if (req$REQUEST_METHOD == "GET") {
-        # handle GET requests
-        query <- parseQueryString(req$QUERY_STRING)
-        # say:
-        # name <- query$name
-        # etc...
-      } 
-      
-      if (req$REQUEST_METHOD == "POST") {
-        # handle POST requests here
-        
-        reqInput <- req$rook.input
-        
-        # read a chuck of size 2^16 bytes, should suffice for our test
-        buf <- reqInput$read(2^16)
-        
-        # simply dump the HTTP request (input) stream back to client
-        shiny:::httpResponse(
-          200, 'text/plain', buf
-        )
-      }          
-    }
-  )
-  
-  # because the API entry is UNIQUE, we need to send it to the client
-  # we can create a custom pipeline to convey this message
-  session$sendCustomMessage("api_url", list(url=api_url))
-  
-  
-}
 
-filter_Get_and_POST =  function(data, req) {
-  print(ls(req))  # you can inspect what variables are encapsulated in this req
-  # environment
-  if (req$REQUEST_METHOD == "GET") {
-    # handle GET requests
-    query <- parseQueryString(req$QUERY_STRING)
-    # say:
-    # name <- query$name
-    # etc...
-  } 
-  
-  if (req$REQUEST_METHOD == "POST") {
-    # handle POST requests here
+GET_PGSCatalog_POSTRequest_metadata <- function(Sample_number){
+    req <- httr::GET(url = paste0("https://www.pgscatalog.org/rest/score/", Sample_number))
+    req_parsed <- httr::content(req, type = "application/json")
     
-    reqInput <- req$rook.input
-    
-    # read a chuck of size 2^16 bytes, should suffice for our test
-    buf <- reqInput$read(2^16)
-    
-    # simply dump the HTTP request (input) stream back to client
-    shiny:::httpResponse(
-      200, 'text/plain', buf
-    )
-  }          
+    return(req_parsed)
 }
